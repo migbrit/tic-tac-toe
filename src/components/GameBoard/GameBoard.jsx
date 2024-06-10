2030f0791b53aced000989573fcd8aa6d863d4d3
Miguel Brito <miguelfelipe_steam@outlook.com>
2024-06-10 02:33:14 -0300

    feat: adding support to letter 'O' in a square, checking if square is null before adding value

--- src/components/GameBoard/GameBoard.jsx
+++ src/components/GameBoard/GameBoard.jsx
@@ -3,32 +3,40 @@ import { useState } from 'react';
 import Square from '../Square/Square';
 import './GameBoard.css';
 
-
 export default function GameBoard(){
+    const [xIsNext, setXIsNext] = useState(true)
     const [squares, setSquares] = useState(Array(9).fill(null))
-    function handleClick(i){
-        const nextSquares = squares.slice();
-        nextSquares[i] = "X";
-        setSquares(nextSquares);
+
+    function handleClick(i, xIsNext){
+        if(squares[i] == null){
+            const nextSquares = squares.slice();
+            if(xIsNext){
+                nextSquares[i] = "X";
+            }else{
+                nextSquares[i] = "O";
+            }
+            setSquares(nextSquares);
+            setXIsNext(!xIsNext); 
+        }
     }
     return (
         <>
             <div className="board-row">
-                <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-                <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-                <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+                <Square value={squares[0]} onSquareClick={() => handleClick(0, xIsNext)} />
+                <Square value={squares[1]} onSquareClick={() => handleClick(1, xIsNext)} />
+                <Square value={squares[2]} onSquareClick={() => handleClick(2, xIsNext)} />
             </div>
 
             <div className="board-row">
-                <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-                <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-                <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+                <Square value={squares[3]} onSquareClick={() => handleClick(3, xIsNext)} />
+                <Square value={squares[4]} onSquareClick={() => handleClick(4, xIsNext)} />
+                <Square value={squares[5]} onSquareClick={() => handleClick(5, xIsNext)} />
             </div>
 
             <div className="board-row">
-                <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-                <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-                <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+                <Square value={squares[6]} onSquareClick={() => handleClick(6, xIsNext)} />
+                <Square value={squares[7]} onSquareClick={() => handleClick(7, xIsNext)} />
+                <Square value={squares[8]} onSquareClick={() => handleClick(8, xIsNext)} />
             </div>
         </>
     )
--- src/components/Square/Square.jsx
+++ src/components/Square/Square.jsx
@@ -1,7 +1,6 @@
 import './Square.css';
 
 export default function Square({value, onSquareClick}){
-
     return (
         <>
             <button className="square" onClick={onSquareClick}>{value}</button>
