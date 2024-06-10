824d55794ca5003f0bf225bd9a650cd2e1d8811f
Miguel Brito <miguelfelipe_steam@outlook.com>
2024-06-10 02:54:16 -0300

    feat: calculating the winner, showing in the screen

--- src/components/GameBoard/GameBoard.jsx
+++ src/components/GameBoard/GameBoard.jsx
@@ -8,19 +8,32 @@ export default function GameBoard(){
     const [squares, setSquares] = useState(Array(9).fill(null))
 
     function handleClick(i, xIsNext){
-        if(squares[i] == null){
-            const nextSquares = squares.slice();
-            if(xIsNext){
-                nextSquares[i] = "X";
-            }else{
-                nextSquares[i] = "O";
-            }
-            setSquares(nextSquares);
-            setXIsNext(!xIsNext); 
+        if(squares[i] || calculateWinner(squares)){
+            return;
         }
+    
+        const nextSquares = squares.slice();
+        if(xIsNext){
+            nextSquares[i] = "X";
+        }else{
+            nextSquares[i] = "O";
+        }
+        setSquares(nextSquares);
+        setXIsNext(!xIsNext); 
+   
     }
+
+    const winner = calculateWinner(squares);
+    let status;
+    if (winner){
+        status = "Winner: " + winner;
+    } else{
+        status = "Next player: " + (xIsNext ? "X" : "O")
+    }
+
     return (
         <>
+            <div className="status">{status}</div>
             <div className="board-row">
                 <Square value={squares[0]} onSquareClick={() => handleClick(0, xIsNext)} />
                 <Square value={squares[1]} onSquareClick={() => handleClick(1, xIsNext)} />
@@ -40,4 +53,25 @@ export default function GameBoard(){
             </div>
         </>
     )
+}
+
+
+function calculateWinner(squares){
+    const winScenariosPositions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+    for(let i = 0; i < winScenariosPositions.length; i++){
+        const [a, b, c] = winScenariosPositions[i]
+        if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
 }
